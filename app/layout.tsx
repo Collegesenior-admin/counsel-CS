f392fd233c0db53778ea32cd0aeba2befaee2111
Collegesenior-admin <admin.collegesenior@gmail.com>
2026-05-14 22:50:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           rel="stylesheet" 
         />
       </head>
-      <body className="font-sans antialiased">
+      <body className="font-poppins antialiased">
         {children}
       </body>
     </html>
