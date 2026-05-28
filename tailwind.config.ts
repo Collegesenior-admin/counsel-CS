d2b7aa69c43d082d9eb0a5fb96467e05aad68549
Collegesenior-admin <admin.collegesenior@gmail.com>
2026-05-28 18:31:20 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,23 +7,45 @@ const config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Poppins"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+      },
+
       screens: {
         '3xl': '1200px',
-        'tablet': '640px',
+        tablet: '640px',
       },
+
       herotext: {
         fontSize: '10px',
         fontWeight: '200',
         lineHeight: '56px',
         color: '#FFFFFF',
       },
+
+      /* ADD THIS */
+      keyframes: {
+        marquee: {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-50%)',
+          },
+        },
+      },
+
+      animation: {
+        'marquee': 'marquee 30s linear infinite',
+      },
     },
-    },
-plugins: [
-  scrollbarHide
-],
+  },
+
+  plugins: [scrollbarHide],
 };
 
 export default config;
