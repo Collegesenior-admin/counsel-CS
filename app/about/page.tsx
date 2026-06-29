12e8baf5ab9a841711507c1e6164f73d76c1504a
Collegesenior-admin <admin.collegesenior@gmail.com>
2026-06-29 14:23:22 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -59,56 +59,56 @@ export default function HomePage() {
         <Header />
 
         <section className="m-3">
-          <div className="relative max-w-340 mx-auto">
+          <div className="relative max-w-355 mx-auto">
 
-            <div className="flex flex-wrap justify-between gap-5 mt-5 mb-10 p-3">
-              <h1 className="flex-1 min-w-75 text-blue-700 leading-tight tracking-wide">
-                <p className="text-sm font-medium opacity-80 ">Home / About</p>
+            <p className="text-md text-blue-700 font-medium opacity-80 tracking-wider ml-4">Home / About</p>
+            <div className="flex flex-wrap justify-between gap-5 mb-10 p-3">
+              <h2 className="flex-1 min-w-75 text-blue-700 leading-tight tracking-widest font-semibold text-[22px] md:text-2xl lg:text-4xl">
                 Your Reliable Partner for College
                 Admissions Guidance
-              </h1>
-              <p className="flex-1 min-w-75 max-w-150 text-justify text-md text-[#777] mt-1">
+              </h2>
+              <p className="flex-1 min-w-75 max-w-150 text-justify items-end text-md text-[#777] mt-1">
                 At CollegeSenior, we re your trusted partner in the college
                 admissions journey. We truly understand the hopes and careful
                 planning of students and parents across Tamil Nadu, and we re here
                 to guide you with clarity, confidence, and complete support every
                 step of the way.            </p>
             </div>
-            <div className="h-80 md:h-135 bg-gray-200 rounded-2xl m-4">
+            <div className="h-80 md:h-135 bg-gray-200 rounded-2xl m-4 mb-8">
               {/* About image */}
             </div>
           </div>
         </section>
 
-        <section className="relative h-auto p-3 m-5 bg-primary rounded-xl text-white">
+        <section className="relative h-auto min-[1345]:h-160 md:p-5 lg:p-8 m-5 bg-primary rounded-xl text-white">
           <div className="p-5 max-w-350 mx-auto">
-            <h2 className="text-md font-bold  text-justify md:w-[80%] lg:w-[80%] md:text-3xl md:font-medium">Empowering students to build the future they deserve at
+            <h2 className="text-md md:leading-9 md:tracking-tight lg:tracking-wide font-medium text-left w-full md:text-[24px] lg:text-[30px]">Empowering students to build the future they deserve at
               CollegeSenior, we are passionate about empowering
               students and parents to take confident control of the
               college admission journey and turn dreams into reality.</h2>
 
-            <p className="text-sm md:text-lg text-justify md:w-[80%] lg:w-[80%] mt-8">At CollegeSenior, we are passionate about empowering students and parents to take confident control of the college admission journey and
+            <p className="text-[10px] md:text-[12px] lg:text-[16px] text-[#f7f7f7] md:tracking-wide md:leading-5 text-justify w-full mt-6 md:mt-18">At CollegeSenior, we are passionate about empowering students and parents to take confident control of the college admission journey and
               turn dreams into reality. We are dedicated to revolutionizing the way families approach higher education in India, delivering clear,
               personalised guidance that opens doors to the very best colleges.</p>
 
 
-            <div className="w-auto my-2 mx-3 text-left">
-              <div className="grid-row gap-4 md:grid-cols-2 p-1">
-                <div className=" w-full flex flex-wrap gap-10 justify-between mt-5">
+            <div className="w-auto text-left mt-8 min-[1345]:mt-35">
+              <div className="grid-row gap-6 md:grid-cols-2 p-1">
+                <div className="w-full flex flex-wrap gap-16 justify-between">
 
                   {stats.map((stat, i) => (
-                    <div key={i} className="flex-1 shrink-0 min-w-62.5 ">
+                    <div key={i} className="flex-1 shrink-0 w-65 min-w-58">
                       <div className="relative inline-block mb-2 w-full">
-                        <span className="flex-1 text-[3rem] w-2/5 md:text-[4rem] font-medium leading-none">
+                        <span className="flex-1 text-[48px] w-2/5 md:text-[4rem] font-medium leading-none">
                           {stat.number}
                         </span>
-                        <span className="flex-1 pb-5 w-3/5 text-[0.75rem] leading-none">
+                        <span className="flex-1 items-start text-[8px]">
                           {stat.labelTop}
                         </span>
 
                       </div>
-                      <p className="text-md leading-relaxed text-justify">
-                        <strong className="font-bold">{stat.title}</strong> – {stat.desc}
+                      <p className="text-sm md:text-md leading-5tracking-[1px] text-left font-normal">
+                        <span className="font-medium">{stat.title}</span> - {stat.desc}
                       </p>
                     </div>
                   ))}
@@ -121,20 +121,20 @@ export default function HomePage() {
         </section>
 
         {/* Counselling Team Section */}
-        <section className="py-10 px-5">
-          <div className="max-w-300 mx-auto">
+        <section className="py-6 md:py-10 px-5">
+          <div className="max-w-350 mx-auto">
 
-            <div className="flex flex-wrap justify-between gap-5 mb-10">
-              <h2 className="flex-1 min-w-75 text-2xl md:text-[1.8rem] font-bold leading-tight tracking-wide">
+            <div className="text-center sm:text-left sm:flex justify-between gap-5 mb-10">
+              <h2 className="flex-1 max-w-180 lg:text-3xl md:text-[22px] text-[20px] text-[#424242] font-semibold mb-4 tracking-widest">
                 Get your counselling from our experienced team
               </h2>
-              <p className="flex-1 min-w-75 max-w-150 text-sm text-[#777] mt-1">
+              <p className="flex-1 max-w-165 text-justify text-sm text-[#777] mt-1 tracking-wider">
                 Our counselors, with years of experience in education and admissions, have helped hundreds of students secure seats in top colleges. Personalized guidance you can trust.
               </p>
             </div>
 
 
-            <div className="flex gap-6 overflow-x-auto scroll-smooth snap-x snap-mandatory max-w-340 w-full pb-4">
+            <div className="flex gap-6 overflow-x-auto scroll-smooth snap-x snap-mandatory max-w-390 w-full justify-between pb-4">
               {team.map((member, i) => (
                 <div key={i} className="shrink-0 snap-start">
                   <ProfileCard {...member} />
@@ -143,28 +143,28 @@ export default function HomePage() {
 
               {/* CTA Card */}
               <div
-                className="shrink-0 snap-start bg-[#1a56db] text-white rounded-xl border p-7 flex flex-col justify-center relative max-h-90"
+                className="shrink-0 snap-start bg-linear-to-br from-blue-500 via-blue-700 to-blue-800 text-white rounded-xl border p-5 flex flex-col justify-center relative max-h-102"
                 style={{
-                  width: "clamp(220px, 80vw, 280px)", // responsive width
+                  width: "clamp(290px, 90vw, 330px)", // responsive width
                   clipPath: `polygon(
         0% 0%,
-        calc(100% - 50px) 0%,
-        calc(100% - 50px) 25px,
-        calc(100% - 25px) 25px,
-        calc(100% - 25px) 50px,
-        100% 50px,
+        calc(100% - 70px) 0%,
+        calc(100% - 70px) 38px,
+        calc(100% - 35px) 38px,
+        calc(100% - 35px) 75px,
+        100% 75px,
         100% 100%,
         0% 100%
       )`,
                 }}
               >
-                <div className="text-xl font-bold mb-4 leading-snug">
+                <div className="text-[18px] font-medium my-auto mb-4 leading-snug">
                   Do you have a quick question or are you feeling overburdened?
                 </div>
-                <div className="text-sm opacity-90 mb-auto leading-snug">
+                <div className="text-sm opacity-90 mb-auto leading-snug tracking-wider text-[#f5f5f5]">
                   Get individualized advice from one of our knowledgeable counselors right away.
                 </div>
-                <button className="mt-5 bg-white text-[#1a56db] font-bold text-sm py-3 rounded-lg hover:bg-gray-100 transition">
+                <button className="mt-5 bg-white text-[#1a56db] font-semibold tracking-wider text-sm py-3 rounded-lg hover:bg-gray-100 transition">
                   Talk to an Expert
                 </button>
               </div>
@@ -174,31 +174,31 @@ export default function HomePage() {
         </section>
 
         {/* Our Offering Section */}
-        < section className=" text-[#333] py-16 px-5" >
-          <div className="max-w-300 mx-auto">
+        < section className=" text-[#333] py-6 md:py-16 px-5" >
+          <div className="max-w-360  mx-auto">
 
             <div className="text-center mb-10">
-              <div className="text-[#1a56db] font-semibold text-sm tracking-widest mb-4">
+              <div className="text-[#1a56db] font-semibold text-md md:text-[23px] tracking-widest mb-4">
                 - Our Offering -
               </div>
-              <h2 className="text-2xl md:text-[1.8rem] font-semibold text-[#444] leading-tight max-w-175 mx-auto">
+              <h2 className="text-[20px] md:text-[32px] font-semibold text-[#444] lg:tracking-widest lg:leading-11.25 max-w-175 mx-auto">
                 Complete Support Across the Entire Admission Ecosystem
               </h2>
             </div>
 
-            <div className="flex justify-center items-center gap-4 my-8 md:flex-row ">
+            <div className="flex justify-center items-center gap-4 my-8 md:flex-row mb-12">
               <span className="font-semibold text-[#1a56db] text-lg">For</span>
               <div className="bg-[#1a56db] rounded-full p-1 flex shadow-lg">
                 <button
                   onClick={() => setTab("students")}
-                  className={`px-8 py-2 rounded-full font-bold transition ${tab === "students" ? "bg-white text-[#1a56db] shadow" : "text-white"
+                  className={`px-8 py-2 rounded-full font-medium transition ${tab === "students" ? "bg-white text-[#1a56db] shadow " : "text-white"
                     }`}
                 >
                   Students
                 </button>
                 <button
                   onClick={() => setTab("colleges")}
-                  className={`px-8 py-2 rounded-full font-bold transition ${tab === "colleges" ? "bg-white text-[#1a56db] shadow" : "text-white"
+                  className={`px-8 py-2 rounded-full font-medium transition ${tab === "colleges" ? "bg-white text-[#1a56db] shadow" : "text-white"
                     }`}
                 >
                   Colleges
@@ -207,20 +207,17 @@ export default function HomePage() {
             </div>
 
             {tab === "students" && (
-
-
-              <div className="flex gap-5 overflow-x-auto scroll-smooth snap-x snap-mandatory pb-4">
+              <div className="flex gap-15 min-[1350px]:justify-center overflow-x-auto scroll-smooth snap-x snap-mandatory pb-4">
                 {students.map((item, i) => (
-                  <div key={i} className="shrink-0  snap-start">
+                  <div key={i} className="shrink-0 snap-start">
                     <OfferingCard index={i} {...item} />
                   </div>
                 ))}
               </div>
-
             )}
 
             {tab === "colleges" && (
-              <div className="flex gap-8 overflow-x-auto scroll-smooth snap-x snap-mandatory w-full pb-4">
+              <div className="flex gap-15 min-[1010px]:justify-center overflow-x-auto scroll-smooth snap-x snap-mandatory w-full pb-4">
                 {colleges.map((item, i) => (
                   <div key={i} className="shrink-0 snap-start">
                     <OfferingCard index={i} {...item} />
@@ -231,8 +228,8 @@ export default function HomePage() {
 
           </div>
         </section >
-        <section className="bg-[#F8F8F8] py-16 w-full font-poppins">
-          <div className="max-w-337 mx-auto px-5">
+        <section className="bg-white py-6 md:py-16 w-full font-poppins">
+          <div className="max-w-355 ml-8 md:ml-21 px-5">
             <h2 className="text-2xl md:text-3xl font-bold text-[#1A1A1A] mb-8">
               What Our Happy Students Say...
             </h2>
@@ -240,7 +237,7 @@ export default function HomePage() {
             {/* Cards Container */}
             <div className="flex gap-5 overflow-x-scroll pb-4" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
               {/* Card 1 */}
-              <div className="shrink-0 w-75 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
+              <div className="shrink-0 w-80 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
                 <div className="mb-5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -267,7 +264,7 @@ export default function HomePage() {
               </div>
 
               {/* Card 2 */}
-              <div className="shrink-0 w-75 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
+              <div className="shrink-0 w-80 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
                 <div className="mb-5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -294,7 +291,7 @@ export default function HomePage() {
               </div>
 
               {/* Card 3 */}
-              <div className="shrink-0 w-75 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
+              <div className="shrink-0 w-80 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
                 <div className="mb-5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -322,7 +319,7 @@ export default function HomePage() {
               </div>
 
               {/* Card 4 */}
-              <div className="shrink-0 w-75 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
+              <div className="shrink-0 w-80 sm:w-87.5 md:w-100 min-h-95 rounded-xl p-8 bg-[#235EE7] text-white flex flex-col">
                 <div className="mb-5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -353,8 +350,8 @@ export default function HomePage() {
 
       </div>
       <Footer />
-      <EnquiryFormModal 
-        isOpen={isModalOpen} 
+      <EnquiryFormModal
+        isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         sourcePage="About Page"
       />
@@ -365,8 +362,8 @@ export default function HomePage() {
 /* Profile Card */
 function ProfileCard({ name, role }: { name: string; role: string; img: string }) {
   return (
-    <div className="w-55 sm:w-70 md:w-70 shrink-0 flex flex-col">
-      <div className="h-90 w-full rounded-xl overflow-hidden mb-4 bg-gray-300 shadow-sm">
+    <div className="w-80 sm:w-80 md:w-82 shrink-0 flex flex-col">
+      <div className="h-102 w-full rounded-xl overflow-hidden mb-4 bg-gray-300 shadow-sm">
         {/* <Image src={img} alt={name} width={400} height={500} className="w-full h-full object-cover" /> */}
       </div>
       <div className="flex justify-between">
